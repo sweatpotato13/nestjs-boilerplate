ce6866417244d5f5b5fb898c6e6a9edd430be2ef
JangChorong <sweatpotato13@gmail.com>
2020-12-06 12:50:10 +0900

    chore: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2018,
+        project: "tsconfig.json",
+        sourceType: "module"
+    },
+    plugins: ["@typescript-eslint/eslint-plugin", "security", "simple-import-sort"],
+    extends: [
+        "plugin:security/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint"
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    root: true,
+    env: {
+        node: true,
+        jest: true
+    },
+    rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "@typescript-eslint/naming-convention": [
+            "error",
+            {
+                "selector": "variableLike",
+                "format": ["strictCamelCase", "UPPER_CASE"]
+            },
+            {
+                "selector": "typeLike",
+                "format": ["PascalCase"]
+            }
+        ]
+    },
+    settings: {
+        "import/resolver": {
+            node: { extensions: [".js", ".jsx", ".ts", ".tsx"] }
+        }
+    }
+};
