ba9c692bca594a441f424691eac07580c20e86fb
Cute_Wisp <sweatpotato13@gmail.com>
2025-01-12 12:49:58 +0900

    test: 💍 fix auth test

--- src/modules/auth/app/auth.controller.spec.ts
+++ src/modules/auth/app/auth.controller.spec.ts
@@ -1,4 +1,6 @@
+import { ConfigType } from "@nestjs/config";
 import { Test, TestingModule } from "@nestjs/testing";
+import { GoogleOauthConfig } from "@src/config";
 import { ResultResponseDto } from "@src/shared/dtos";
 import httpMocks from "node-mocks-http";
 
@@ -10,12 +12,24 @@ const mockAuthService: Partial<AuthService> = {
     googleLogin: jest.fn()
 };
 
+const mockGoogleConfig: ConfigType<typeof GoogleOauthConfig> = {
+    clientId: "mock-client-id",
+    clientSecret: "mock-client-secret",
+    redirect: "mock-redirect"
+};
+
 describe("AuthController", () => {
     let authController: AuthController;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [{ provide: "AuthService", useValue: mockAuthService }],
+            providers: [
+                { provide: "AuthService", useValue: mockAuthService },
+                {
+                    provide: GoogleOauthConfig.KEY,
+                    useValue: mockGoogleConfig
+                }
+            ],
             controllers: [AuthController]
         }).compile();
 
@@ -23,7 +37,7 @@ describe("AuthController", () => {
     });
 
     describe("GET /auth/login/google", () => {
-        it("should return result", () => {
+        it("should return result when Google auth is enabled", () => {
             const resp = ResultResponseDto.of({
                 result: "Google Authentication"
             });
@@ -33,7 +47,7 @@ describe("AuthController", () => {
     });
 
     describe("GET /auth/login/google/callback", () => {
-        it("should return healthCheck", async () => {
+        it("should return tokens when Google auth is enabled", async () => {
             const args = httpMocks.createRequest({
                 method: "GET",
                 url: "/auth/google/callback",
@@ -49,9 +63,7 @@ describe("AuthController", () => {
                 refreshToken: "refreshToken"
             });
 
-            jest.spyOn(mockAuthService, "googleLogin").mockImplementation(() =>
-                Promise.resolve(resp)
-            );
+            jest.spyOn(mockAuthService, "googleLogin").mockResolvedValue(resp);
 
             expect(await authController.handleRedirect(args)).toBe(resp);
         });
